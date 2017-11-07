367e58fe59649f8c73b60b66a97ee6fc43b124cb
reidnaa <reidnaaykens@yahoo.ca>
2017-11-07 03:04:36 +0000

    Support Command Line Arguments

--- download_avatars.js
+++ download_avatars.js
@@ -2,11 +2,58 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+
+const owner = process.argv[2];
+const repo = process.argv[3];
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+
+
+
+
+function downloadImageByURL(url, filePath) {
+
+  request.get(url);
+
+  request.pipe(fs.createWriteStream(filePath));
+
+   };
+
+
+
+
+
+getRepoContributors(owner, repo, function(err, result) {
+
+  if (!owner || !repo) {
+    console.log("owner and repo name in node");
+    return;
+  }
+
+  const contributors = JSON.parse(result);
+
+
+
+  contributors.forEach(function(contributor) {
+
+
+    let url = contributor.avatar_url;
+
+    let path = `./avatars/${contributor.login}.jpg`;
+
+
+    downloadImageByURL(url, path);
+  });
 });
